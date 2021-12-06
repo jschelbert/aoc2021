d79f5ccc1615fbe2c6fe1a0800f02026e53374fc
Jakob Schelbert <jakob.schelbert@deutschebahn.com>
2021-12-06 12:14:50 +0100

    Day 1 part 2

--- 01/aoc2021_01.py
+++ 01/aoc2021_01.py
@@ -1,6 +1,7 @@
 overall_increase = 0
 temp_num = 1000000000
 
+print("Part 1")
 with open("input.txt", "r") as f:
     numbers = f.read()
 
@@ -10,4 +11,19 @@ with open("input.txt", "r") as f:
             overall_increase += 1
         temp_num = num
 
+    print(f"There were {overall_increase} increases")
+
+
+overall_increase = 0
+temp_num = 1000000000
+
+print("Part 2")
+with open("input.txt", "r") as f:
+    numbers = f.read()
+    numbers = [int(line) for line in numbers.splitlines()]
+    sliding_window = [sum(numbers[i:i+3]) for i in range(0, len(numbers)-2)]
+    for num in sliding_window:
+        if num > temp_num:
+            overall_increase += 1
+        temp_num = num
     print(f"There were {overall_increase} increases")
