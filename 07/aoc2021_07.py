40cca6ceb170a0c44b4f593561ad017428d513b2
Jakob Schelbert <jakob.schelbert@deutschebahn.com>
2021-12-08 15:02:10 +0100

    Day 7 part 2

--- 07/aoc2021_07.py
+++ 07/aoc2021_07.py
@@ -5,3 +5,9 @@ my_data = np.loadtxt('input.txt', delimiter=',')
 best_height = np.median(my_data)
 
 print(f"{sum(abs(my_data - best_height))}")
+
+
+print("Part 2")
+best_height2 = np.floor(np.mean(my_data))
+
+print(f"{sum([j for i in abs(my_data - best_height2) for j in range(1, int(i)+1)])} or {sum([j for i in abs(my_data - best_height2 - 1) for j in range(1, int(i)+1)])}")
